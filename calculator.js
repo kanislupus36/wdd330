9feaf69f2cfd2c2ccf492133688e236f33db8e30
kadin <kadinpadams@gmail.com>
2025-01-07 20:57:09 -0700

    calculator

---
+++ calculator.js
@@ -0,0 +1,33 @@
+let calculator = {
+    sum()
+    {
+        return this.a + this.b;
+    },
+
+    mul()
+    {
+        return this.a * this.b;
+    },
+
+    sub()
+    {
+        return this.a - this.b;
+    },
+
+    div()
+    {
+        return this.a / this.b;
+    },
+
+    read: function(num1, num2)
+    {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
+  alert( calculator.sub() );
+  alert( calculator.div() );
